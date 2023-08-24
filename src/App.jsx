62b4ff17b86ed07469888981eebee5ec96a01f5d
Marian <hellomariandev@gmail.com>
2023-08-24 21:44:36 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+
 function App() {
-  return <>Hi</>;
+  const [text] = useTypewriter({
+    words: ["Developer", "Designer"],
+    loop: {},
+    typeSpeed: 110,
+    deleteSpeed: 60,
+  });
+
+  return (
+    <>
+      <h1 style={{ margin: "5px" }}>
+        I'm a <span style={{ fontWeight: "bold", color: "green" }}>{text}</span>.
+      </h1>
+      <span style={{ color: "red" }}>
+        <Cursor />
+      </span>
+    </>
+  );
 }
 
 export default App;
