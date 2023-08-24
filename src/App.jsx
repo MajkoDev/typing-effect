e78020e893add220063cf1f2085d44085cefdc48
Marian <hellomariandev@gmail.com>
2023-08-24 21:47:21 +0200

    react simple typewriter

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
-import { useTypewriter, Cursor } from "react-simple-typewriter";
+import ReactSimpleTypewriter from "./components/ReactSimpleTypewriter";
 
 function App() {
-  const [text] = useTypewriter({
-    words: ["Developer", "Designer"],
-    loop: {},
-    typeSpeed: 110,
-    deleteSpeed: 60,
-  });
 
   return (
     <>
-      <h1 style={{ margin: "5px" }}>
-        I'm a <span style={{ fontWeight: "bold", color: "green" }}>{text}</span>.
-      </h1>
-      <span style={{ color: "red" }}>
-        <Cursor />
-      </span>
+
+<ReactSimpleTypewriter />
+
     </>
   );
 }
---
+++ src/components/ReactSimpleTypewriter.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+
+const ReactSimpleTypewriter = () => {
+  const [text] = useTypewriter({
+    words: ["Developer", "Designer"],
+    loop: {},
+    typeSpeed: 110,
+    deleteSpeed: 60,
+  });
+
+  return (
+    <>
+      <h1 style={{ margin: "5px" }}>
+        I'm a <span style={{ fontWeight: "bold", color: "green" }}>{text}</span>
+        .
+      </h1>
+      <span style={{ color: "red" }}>
+        <Cursor />
+      </span>
+    </>
+  );
+};
+
+export default ReactSimpleTypewriter;
